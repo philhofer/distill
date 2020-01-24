a94f0c4722ed574b70e6b7545a428a32f20c3cc5
philhofer <phofer@umich.edu>
2020-01-24 09:45:04 -0800

    pkg/flex: fix
    Now that (import (pkg ...)) works, make sure all the packages in pkg/
    actually load correctly.

--- pkg/flex.scm
+++ pkg/flex.scm
@@ -1,15 +1,26 @@
-(define flex
-  (let* ((version '2.6.4)
-         (leaf    (remote-archive
-                    (conc "https://github.com/westes/flex/releases/download/v" version "/flex-" version ".tar.gz")
-                    "iyTO0NJ3ype1atS2SbQkeEA24JzbugW3OCxyRQVBBCw=")))
-    (package-lambda
-      conf
-      (make-package
-        #:label  (conc "flex-" version "-" (conf 'arch))
-        #:src    leaf
-        ;; NOTE: flex builds some binaries for the build system
-        ;; as part of bootstrapping, so it may need 2 C toolchains
-        #:tools  (append (list m4 bison musl) (cc-for-target conf))
-        #:inputs (list musl)
-        #:build  (gnu-build (conc "flex-" version) conf)))))
+(module (pkg flex)
+  (flex)
+  (import
+    scheme
+    (base)
+    (package)
+    (plan)
+    (only (chicken string) conc)
+    (pkg bison))
+
+  (define flex
+    (let* ((version '2.6.4)
+           (leaf    (remote-archive
+                      (conc "https://github.com/westes/flex/releases/download/v" version "/flex-" version ".tar.gz")
+                      "iyTO0NJ3ype1atS2SbQkeEA24JzbugW3OCxyRQVBBCw=")))
+      (lambda (conf)
+        (make-package
+          #:label  (conc "flex-" version "-" (conf 'arch))
+          #:src    leaf
+          ;; NOTE: flex builds some binaries for the build system
+          ;; as part of bootstrapping, so it may need 2 C toolchains
+          #:tools  (append (list m4 bison musl) (cc-for-target conf))
+          #:inputs (list musl)
+          #:build  (gnu-build (conc "flex-" version) conf)))))
+
+  )
