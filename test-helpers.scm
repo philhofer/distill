d00f8e89cf4dc2c19ac93d889b58a3007887bee7
philhofer <phofer@umich.edu>
2020-01-24 21:29:58 -0800

    test-helpers: add (test* ...) form
    Add syntax called test* that builds a table of tests.
    Cuts down on some boilerplate here and in some upcoming changes.

--- test-helpers.scm
+++ test-helpers.scm
@@ -8,3 +8,10 @@
        (unless (cmp? val got)
 	 (error "test failure:" (quote expr) "should be" val "but is" got))))))
 
+(define-syntax test*
+  (syntax-rules ()
+    ((_ proc ((k vals ...) ...))
+     (test* equal? proc ((k vals ...) ...)))
+    ((_ cmp? proc ((k vals ...) ...))
+     (begin
+       (test cmp? k (proc vals ...)) ...))))
