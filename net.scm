1b54e533b739497503537745b33b41bdc42c82df
philhofer <phofer@umich.edu>
2020-04-28 08:27:52 -0700

    net: remove dead code
    Pretty much all of net.scm is dead at this point;
    svc/auto-dhcpcd covers most of the functionality
    one would want.

--- net.scm
+++ net.scm
@@ -8,34 +8,3 @@
               down: `((fdmove -c 2 1)
                       (foreground ((ip link set dev lo down)))
                       (true)))))
-
-(define (el-null-or x)
-  (if (null? x) '() `((if (,x)))))
-
-(define (el-end-or expr rest)
-  (if (null? rest)
-    (list expr)
-    `((foreground (,expr)) ,@rest)))
-
-(define (netif name #!key
-               (addrs     '())
-               (pre-up    '())
-               (post-up   '())
-               (pre-down  '())
-               (post-down '())
-               (after     '()))
-  (make-service
-    name:   (string->symbol (conc "net." name))
-    inputs: (list iproute2)
-    after:  after
-    spec:   (oneshot*
-              up:   `((fdmove -c 2 1)
-                      ,@(map
-                          (lambda (addr)
-                            `(ip address add ,addr dev ,name))
-                          addrs)
-                      ,@(el-null-or pre-up)
-                      ,@(el-end-or `(ip link set dev ,name up) post-up))
-              down: `((fdmove -c 2 1)
-                      ,@(el-null-or pre-down)
-                      ,@(el-end-or `(ip link set dev ,name down) post-down)))))
