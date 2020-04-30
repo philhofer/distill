3b2def4aca23270b626c5be8e0207cdd1d5a19b6
philhofer <phofer@umich.edu>
2020-04-29 21:47:57 -0700

    pkg/ip-wait: new package
    Add a utility script for waiting on networking events.
    
    This covers the simple cases where services need to
    wait for networking to be 'up' (for some definition of up)
    before starting in earnest. Searching for a pattern in
    ip(8) output keeps us from having to make a global decision
    about what it means for the network to be 'up,' and also
    keeps the explicit service dependencies much simpler,
    since we don't have to encode the notion of networking states
    into the service DAG.
    
    By default, wait for a default route to be installed
    before starting ntpd.

---
+++ pkg/ip-wait.scm
@@ -0,0 +1,51 @@
+(import
+  scheme
+  (distill execline)
+  (distill plan))
+
+;; ip-wait is a script for waiting
+;; until a particular networking-related
+;; event has happend (as defined by a pattern
+;; occuring on the output of 'ip show' or 'ip monitor')
+;;
+;; usage:
+;;   ip-wait <event-kind> <regex>
+;;
+;; examples:
+;;
+;;  # # wait for a globally-routable address:
+;;  # ip-wait addr 'inet.*scope global'
+;;  # # wait for a default route:
+;;  # ip-wait route '^default'
+;;  # # wait for wlan0 interface hotplug:
+;;  # ip-wait link '^[0-9]+: wlan0:'
+;;  # # wait for a neighbor:
+;;  # ip-wait neigh 'lladdr aa:bb:cc:dd:ee:ff REACHABLE$'
+;;
+(define ip-wait
+  (interned
+   "/bin/ip-wait" #o755
+   (lambda ()
+     (write-exexpr
+      ;; the reason why this is so nasty
+      ;; is because there isn't a way to get
+      ;; 'ip monitor' to show the current state,
+      ;; and because there is a race we need to
+      ;; work around between calling 'ip monitor'
+      ;; and 'ip ... show' where the event could happen
+      '((piperw 3 4)
+	(background ((redirfd -r 0 /dev/null)
+		     (fdclose 3)
+		     (fdmove 1 4)
+		     (ip monitor "$1")))
+	(importas |-i| -u mon !)
+	(fdclose 4)
+	(pipeline ((foreground ((ip show "$1")))
+		   (fdmove 0 3)
+		   (cat)))
+	(importas |-i| -u bg !)
+	(if ((grep -m1 -E "$2")))
+	(if ((kill "$bg")))
+	(if ((kill "$mon")))
+	(wait |-i| (("$bg" "$mon"))))
+      shebang: "#!/bin/execlineb -s2"))))
--- svc/ntpd.scm
+++ svc/ntpd.scm
@@ -8,6 +8,7 @@
   (distill unix)
   (only (distill sequence) ->lines+spaces)
 
+  (pkg ip-wait)
   (pkg openntpd))
 
 (export default-ntpd-config)
@@ -24,10 +25,13 @@
 		    (->lines+spaces lines))))
     (make-service
      name:   'ntpd
-     inputs: (list openntpd conf)
+     inputs: (list openntpd ip-wait conf)
      users:  (list (adduser 'ntpd group: 'ntpd home: "/var/empty"))
      groups: (list (addgroup 'ntpd '(ntpd)))
      after:  (list var-mounted-rw)
      spec:   (longrun*
 	      run: `((fdmove -c 2 1)
+		     ;; no use starting until we have a default route:
+		     (if ((s6-setuidgid nobody)
+			  (ip-wait route "^default")))
 		     (/usr/sbin/ntpd -s -d -f ,confpath))))))
