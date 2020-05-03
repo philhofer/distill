43391764fac12495db33ea1bd0d88179d144dc73
philhofer <phofer@umich.edu>
2020-05-02 17:17:11 -0700

    pkg/i2c-tools: new package

---
+++ pkg/i2c-tools.scm
@@ -0,0 +1,15 @@
+(import
+  scheme
+  (distill base)
+  (distill package))
+
+(define i2c-tools
+  (cc-package
+   "i2c-tools" "4.1"
+   "https://mirrors.edge.kernel.org/pub/software/utils/$name/$name-$version.tar.gz"
+   "gRACB_gv_oz_gszxCsu1E1nrPfS5tFHejfNd0VzED3s="
+   env: (lambda (conf)
+	  (list ($cc-env conf)))
+   libs: (list linux-headers)
+   build: '((if ((make DESTDIR=/out PREFIX=/usr BUILD_STATIC_LIB=1 BUILD_DYNAMIC_LIB=0 install)))
+	    (rm -rf /out/usr/share))))
