925985486d3a17af2f359f0bfd6032c55b057b1f
philhofer <phofer@umich.edu>
2020-05-12 10:08:35 -0700

    pkg/mtools: new package
    Working on packaging stuff that will let
    us produce a UEFI ESP image without actually
    mounting a vfat filesystem.

---
+++ pkg/mtools.scm
@@ -0,0 +1,9 @@
+(import
+  scheme
+  (distill package))
+
+(define mtools
+  (cmmi-package
+   "mtools" "4.0.24"
+   "https://ftp.gnu.org/gnu/$name/$name-$version.tar.gz"
+   "TDTnHIdZ2Sr-dDRK4r7Jus0HR0WaSkVypAykJJAsU0o="))
