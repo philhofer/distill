b0048a82fec0286d31cfa19ff4bdfbbbdfdfb0d6
philhofer <phofer@umich.edu>
2020-05-16 16:18:10 -0700

    pkg/jq: new package
    This will be necessary for some of the
    Digital Ocean droplet scripts (for parsing the
    droplet metadata in order to bring up networking, etc.)

---
+++ pkg/jq.scm
@@ -0,0 +1,19 @@
+(import
+  scheme
+  (chicken module)
+  (distill package))
+
+(export libjq)
+
+(define %jq
+  (cmmi-package
+   "jq" "1.6"
+   "https://github.com/stedolan/$name/releases/download/$name-$version/$name-$version.tar.gz"
+   "8CAQoS4P-ztifJlc_ZE0FI0P_Hq8v4aAH0E655zjJ4w="
+   extra-configure: '(--enable-all-static
+		      --disable-maintainer-mode
+		      --disable-docs
+		      --with-oniguruma=no)))
+
+(define jq (binaries %jq))
+(define libjq (libs %jq))
