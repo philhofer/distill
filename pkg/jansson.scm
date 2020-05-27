76230c77ef50dfb6e51a5ffb6405d74daaf903a9
philhofer <phofer@umich.edu>
2020-05-27 09:15:55 -0700

    nftables: new package

---
+++ pkg/jansson.scm
@@ -0,0 +1,9 @@
+(import
+  scheme
+  (distill package))
+
+(define jansson
+  (cmmi-package
+   "jansson" "2.13.1"
+   "https://digip.org/jansson/releases/$name-$version.tar.gz"
+   "KjlNqNjgMQ-LC47KwidsGWBvPoq-t6-cFRdqTKjusCE="))
