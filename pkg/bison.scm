92a0be11a5b9a5d967b46523494f055e44a01434
Phil Hofer <phil@sunfi.sh>
2020-10-07 21:01:07 +0000

    pkg/bison: fix ppc64le build (upgrade to 3.7.2)
    The native ppc64le build of bison is busted somehow.
    Upgrading to 3.7.2 fixes the issue, so I didn't bother
    digging any deeper.

--- pkg/bison.scm
+++ pkg/bison.scm
@@ -5,8 +5,8 @@
 
 (define bison
   (cmmi-package
-   "bison" "3.6.4"
+   "bison" "3.7.2"
    "https://ftp.gnu.org/gnu/$name/$name-$version.tar.gz"
-   "CqWBAC5QpYjWhY0DQg1N1FjfsSWwikuKoS1l_oxPs-o="
+   "Dki4-d9OhRdte4oRORDCoXvYOoaQoUOhVQbTsPZOAkc="
    tools: (list m4 perl)))
 
