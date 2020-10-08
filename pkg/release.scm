bc0a1d80a89304eca657c602358b49e9365f9c62
Phil Hofer <phil@sunfi.sh>
2020-10-08 04:57:56 +0000

    pkg/release: update to latest snapshot
    This release snapshot makes scripts/build-world.scm
    happy with a clean build from scratch on ppc64le.

--- pkg/release.scm
+++ pkg/release.scm
@@ -14,26 +14,26 @@
 	    (cons head (without syms (cdr lst)))))))
 
 (define release
-  (let ((hash    "0lJoG9e0WHdBM1xxaCjQkoelX6Kw_1P9X37h8iAZhO4=")
-	($cflags (lambda (conf)
-		   ;; due to chicken's compilation model, essentially every
-		   ;; continuation is noreturn, so the stack protector does
-		   ;; literally nothing
-		   (without '(-fstack-protector -fstack-protector-strong -fstack-protector-all)
-			    ($CFLAGS conf)))))
+  (let ((hash "Id7BUW-Zw9XX8aWJiTUxelmZ_fjiyicmn3SVD-6VUXE=")
+	      ($cflags (lambda (conf)
+		               ;; due to chicken's compilation model, essentially every
+		               ;; continuation is noreturn, so the stack protector does
+		               ;; literally nothing
+		               (without '(-fstack-protector -fstack-protector-strong -fstack-protector-all)
+			                      ($CFLAGS conf)))))
     (cc-package
-     "distill" "c5f3143"
+     "distill" "6020b61"
      (string-append
-      "https://b2cdn.sunfi.sh/files/pub-cdn/" hash)
+      "https://b2cdn.sunfi.sh/file/pub-cdn/" hash)
      hash
      tools:  (list chicken)
      build:  `(make DESTDIR=/out
-		PREFIX=/usr
-		CSI=/usr/bin/csi
-		CHICKEN=/usr/bin/chicken
-		,(el= 'CHICKEN_FEATURES= $chicken-features)
-		,(el= 'AR= $AR)
-		,(el= 'CC= $CC)
-		,(el= 'CFLAGS= $cflags)
-		,(el= 'LDFLAGS= $LDFLAGS)
-		install))))
+		            PREFIX=/usr
+		            CSI=/usr/bin/csi
+		            CHICKEN=/usr/bin/chicken
+		            ,(el= 'CHICKEN_FEATURES= $chicken-features)
+		            ,(el= 'AR= $AR)
+		            ,(el= 'CC= $CC)
+		            ,(el= 'CFLAGS= $cflags)
+		            ,(el= 'LDFLAGS= $LDFLAGS)
+		            install))))
