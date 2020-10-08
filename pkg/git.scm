b6493667884765b9eb1e31764a2f06ddb63354b2
Phil Hofer <phil@sunfi.sh>
2020-10-08 19:23:53 +0000

    pkg/git: upgrade 2.27.0 -> 2.28.0

--- pkg/git.scm
+++ pkg/git.scm
@@ -13,19 +13,19 @@
 ;; of git may not be available for a cross-compiled target...
 (define git
   (cmmi-package
-   "git" "2.27.0"
+   "git" "2.28.0"
    "https://www.kernel.org/pub/software/scm/$name/$name-$version.tar.gz"
-   "Gn0ijcbRn9bIcpOYASfwQPTPzxpuqM5a42aG---ZJEI="
+   "CwqCRQt4CR90qdJ4cRgl2ies6h4SJnEHP7RusBbw4yI="
    tools: (list tar)
    libs:  (list pcre2 zlib libexpat libressl)
    ;; necessary for cross-compilation:
    env:   '((ac_cv_fread_reads_directories . yes)
-	    (ac_cv_snprintf_returns_bogus . no))
+	          (ac_cv_snprintf_returns_bogus . no))
    override-make: `(,$make-overrides NO_GETTEXT=1)
    override-install: '(NO_GETTEXT=1 DESTDIR=/out install)
    extra-configure: '(--with-curl
-		      --with-openssl --with-expat
-		      --with-libpcre2 --without-tcltk
-		      --without-iconv
-		      --with-perl=/usr/bin/perl
-		      --with-python=/usr/bin/python3)))
+		                  --with-openssl --with-expat
+		                  --with-libpcre2 --without-tcltk
+		                  --without-iconv
+		                  --with-perl=/usr/bin/perl
+		                  --with-python=/usr/bin/python3)))
