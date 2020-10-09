5340f2d135ac43cee12f50747cceebf892311129
Phil Hofer <phil@sunfi.sh>
2020-10-09 00:12:17 +0000

    pkg/openssh: upgrade 8.3p1 -> 8.4p1

--- pkg/openssh.scm
+++ pkg/openssh.scm
@@ -8,25 +8,25 @@
 
 (define openssh
   (cmmi-package
-   "openssh" "8.3p1"
+   "openssh" "8.4p1"
    "https://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/$name-$version.tar.gz"
-   "6V6bSpzwnMOaF-0R6fQRFmnKXlKFdjjancT_phfoq7M="
+   "IxxcBQ_zvs3h6jBHmc4x8QVUrnebAph82dXx4lyVAuE="
    patches: (patchfiles* "patches/openssh/fix-dns.patch")
    libs: (list linux-headers libedit ncurses zlib libressl)
    extra-configure: '(--with-pid-dir=/run
-		      --disable-lastlog
-		      --disable-strip
-		      --disable-wtmp
-		      --disable-pkcs11 ; requires dlopen() ;
-		      --disable-sk
-		      --with-pie=no ; don't pass -pie; use CFLAGS! ;
-		      --with-sandbox=seccomp_filter ; fail configure if we can't use seccomp
-		      --with-privsep-path=/var/empty
-		      --with-xauth=/usr/bin/xauth
-		      --with-privsep-user=sshd
-		      --with-md5-passwords
-		      --with-libedit)
+		                  --disable-lastlog
+		                  --disable-strip
+		                  --disable-wtmp
+		                  --disable-pkcs11  ; requires dlopen() ;
+		                  --disable-sk
+		                  --with-pie=no     ; don't pass -pie; use CFLAGS! ;
+		                  --with-sandbox=seccomp_filter ; fail configure if we can't use seccomp
+		                  --with-privsep-path=/var/empty
+		                  --with-xauth=/usr/bin/xauth
+		                  --with-privsep-user=sshd
+		                  --with-md5-passwords
+		                  --with-libedit)
    cleanup: '( ;; do NOT keep config files;
-	      ;; those are inserted via overlay
-	      if (rm -rf /out/var)
-	      rm -rf /out/etc)))
+	            ;; those are inserted via overlay
+	            if (rm -rf /out/var)
+	            rm -rf /out/etc)))
