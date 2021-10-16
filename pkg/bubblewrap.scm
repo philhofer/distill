bb6a082de3e3d6b53d4575e144d92e26bcc13816
Phil Hofer <phofer@umich.edu>
2021-10-16 10:20:50 -0700

    bubblewrap: upgrade 0.4.1 -> 0.5.0

--- pkg/bubblewrap.scm
+++ pkg/bubblewrap.scm
@@ -6,9 +6,11 @@
 
 (define bubblewrap
   (cmmi-package
-   "bubblewrap" "0.4.1"
+   "bubblewrap" "0.5.0"
    "https://github.com/containers/$name/releases/download/v$version/$name-$version.tar.xz"
-   "yqpyqB-H9jkcKQyM4vivRReAwzUZGgsNP_HPACJ8gDQ="
+   "wnuJ6h4Fx4-ZQ8ZBsJ3a5pWCdCwO4-R1sh5awvDxpZM="
    libs: (list libcap linux-headers)
-   extra-configure: '(--disable-selinux --disable-man --with-bash-completion-dir=no)))
-
+   extra-configure: '(--disable-selinux
+                      --disable-man
+                      --with-bash-completion-dir=no
+                      --with-zsh-completion-dir=no)))
