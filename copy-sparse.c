b144ea7708900f71c65fdb9a76b8ca94f4e511fc
Phil Hofer <phofer@umich.edu>
2022-08-18 12:06:35 +0000

    copy-sparse: add workaround for latest kernels
    The latest LTS kernels have taken a patch[1] that
    changes the behavior of copy_file_range to reject
    certain cross-filesystem copies with EXDEV.
    So, we have to have a fall-back for that case.
    
    [1]: https://lkml.indiana.edu/hypermail/linux/kernel/2107.0/00910.html

--- copy-sparse.c
+++ copy-sparse.c
@@ -16,6 +16,37 @@
 
 #define really(expr) while ((expr)<0 && errno == EINTR)
 
+/* same as copy_file_range(), but slow and generic */
+static ssize_t
+copy_file_range_fallback(int fdfrom, off64_t *off_in,
+                         int fdto, off64_t *off_out,
+                         size_t len, unsigned int flags)
+{
+    char buf[8192];
+    ssize_t r, p, out;
+    size_t mv;
+
+    out = 0;
+    while (len) {
+        mv = sizeof(buf);
+        if (mv > len)
+            mv = len;
+        really(r = pread(fdfrom, buf, mv, *off_in));
+        if (r == 0)
+            return out; /* eof? */
+        if (r < 0)
+            return -1;
+        really(p = pwrite(fdto, buf, r, *off_out));
+        if (p < 0)
+            return -1;
+        *off_in += p;
+        *off_out += p;
+        out += p;
+        len -= p;
+    }
+    return out;
+}
+
 /* copy_file_sparse() either renames 'from' to
  * 'to,' or copies a sparse file and takes care
  * to preserve holes in the file */
@@ -28,23 +59,26 @@ copy_file_sparse(const char *from, const char *to, bool canrename)
     int err = 0;
     off_t off, off2, end, sz;
     ssize_t ln;
+    bool cpr = true;
 
     /* easy case: just rename */
     if (canrename) {
-	really(err = rename(from, to));
-	if (!err) return 0;
-	err = 0;
+        really(err = rename(from, to));
+        if (!err) return 0;
+        errno = 0;
+        err = 0;
     }
 
     really(fdfrom = open(from, O_RDONLY|O_CLOEXEC));
-    if (fdfrom == -1)
-	return -errno;
+    if (fdfrom == -1) {
+        return -errno;
+    }
 
     really(fdto = open(to, O_CREAT|O_CLOEXEC|O_WRONLY|O_EXCL, 0644));
     if (fdto == -1) {
-	err = errno;
-	close(fdfrom);
-	return -err;
+        err = errno;
+        close(fdfrom);
+        return -err;
     }
 
     really(err = fstat(fdfrom, &stbuf));
@@ -58,34 +92,44 @@ copy_file_sparse(const char *from, const char *to, bool canrename)
     /* for each section of actual data in the file,
      * use copy_file_range to move the data into the new file */
     while (off < sz) {
-	really(off = lseek(fdfrom, off, SEEK_DATA));
-	if (off < 0) {
-	    if (errno == ENXIO) {
-		/* see the man pages for lseek(2):
-		 * ENXIO can occur if we are inside a hole
-		 * at the end of the file (in which case we are done) */
-		off = sz;
-	    } else {
-		err = -errno;
-		goto end;
-	    }
-	}
-	if (off == sz)
-	    break;
-	really(end = lseek(fdfrom, off, SEEK_HOLE));
-	if (end < 0) {
-	    err = -errno;
-	    goto end;
-	}
-	off2 = off;
-	while (off < end) {
-	    really(ln = copy_file_range(fdfrom, &off, fdto, &off2, end-off, 0));
-	    if (ln < 0) {
-		err = -errno;
-		goto end;
-	    }
-	}
-	assert(off == end);
+        really(off = lseek(fdfrom, off, SEEK_DATA));
+        if (off < 0) {
+            if (errno == ENXIO) {
+            /* see the man pages for lseek(2):
+             * ENXIO can occur if we are inside a hole
+             * at the end of the file (in which case we are done) */
+                off = sz;
+            } else {
+                err = -errno;
+                goto end;
+            }
+        }
+        if (off == sz)
+            break;
+        really(end = lseek(fdfrom, off, SEEK_HOLE));
+        if (end < 0) {
+            err = -errno;
+            goto end;
+        }
+        off2 = off;
+        while (off < end) {
+            /* copy_file_range can return EXDEV if we are copying across filesystems */
+            if (cpr) {
+                really(ln = copy_file_range(fdfrom, &off, fdto, &off2, end-off, 0));
+            } else {
+                really(ln = copy_file_range_fallback(fdfrom, &off, fdto, &off2, end-off, 0));
+            }
+            if (ln < 0) {
+                if (cpr && errno == EXDEV) {
+                    cpr = false;
+                    errno = 0;
+                    continue;
+                }
+                err = -errno;
+                goto end;
+            }
+        }
+        assert(off == end);
     }
     assert(off == sz);
  end:
